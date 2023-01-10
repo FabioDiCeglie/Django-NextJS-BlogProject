7e9abf092e4194c13ac2d9c65614fd9806eb3d63
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-10 20:39:21 +0100

    set up serializer, view and first endpoint

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 
 # Create your views here.
-def main(request):
-    return HttpResponse("Hello")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
