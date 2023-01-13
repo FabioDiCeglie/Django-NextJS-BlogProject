a05026f2d10e860134b54d5a86aff430c9fc19d9
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-13 16:25:02 +0100

    chore: adding serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Room
+from .models import Room, Article
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+
+class ArticleSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ("title", "description")
