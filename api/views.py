162b1738b55da6d8984582b88aee96ed283e4813
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-13 17:06:31 +0100

    improving code in views

--- api/views.py
+++ api/views.py
@@ -1,52 +1,93 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Article
 from .serializers import ArticleSerialize
 
 # Create your views here.
-@csrf_exempt
-def article_list(request):
+# @csrf_exempt
 
+@api_view(["GET", "POST"])
+def article_list(request):
     #get all articles
     if request.method == "GET":
         articles = Article.objects.all()
         serializer = ArticleSerialize(articles, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = ArticleSerialize(data=data)
+        serializer = ArticleSerialize(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(["GET", "POST"])
 def article_details(request, pk):
     try:
         article = Article.objects.get(pk=pk)
 
     except Article.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
         serializer = ArticleSerialize(article)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = ArticleSerialize(data=data)
+        serializer = ArticleSerialize(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
         article.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+# def article_list(request):
+
+#     #get all articles
+#     if request.method == "GET":
+#         articles = Article.objects.all()
+#         serializer = ArticleSerialize(articles, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     elif request.method == "POST":
+#         data = JSONParser().parse(request)
+#         serializer = ArticleSerialize(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=201)
+#         return JsonResponse(serializer.errors, status=400)
+
+# @csrf_exempt
+# def article_details(request, pk):
+#     try:
+#         article = Article.objects.get(pk=pk)
+
+#     except Article.DoesNotExist:
+#         return HttpResponse(status=404)
+
+#     if request.method == "GET":
+#         serializer = ArticleSerialize(article)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     elif request.method == "PUT":
+#         data = JSONParser().parse(request)
+#         serializer = ArticleSerialize(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=400)
+
+#     elif request.method == "DELETE":
+#         article.delete()
+#         return HttpResponse(status=204)
 
 # class ArticleView(generics.ListAPIView):
 #     queryset = Article.objects.all()
