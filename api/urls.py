bb6b9068b67a664d0c2d038c07705aafa3013116
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-13 16:56:36 +0100

    create a path for article details

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import  article_list
+from .views import  article_list, article_details
 
 urlpatterns = [
-    path("articles", article_list)
+    path("articles", article_list),
+    path("articles/<int:pk>/", article_details)
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-from django.views.decorators import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from rest_framework import generics
 from .models import Article
