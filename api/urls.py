2bb0ff1884cc343fb34f1da1fd357503c4adf20c
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-16 22:25:23 +0100

    register urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
-from .views import  ArticleList, ArticleDetails
+from django.urls import path,include
+# from .views import  ArticleList, ArticleDetails
+from .views import ArticleViewSet
+from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 # article_list, article_details
 
+router = routers.DefaultRouter()
+router.register(r'articles', ArticleViewSet, basename='articles')
+
 urlpatterns = [
-    path("articles", ArticleList.as_view()),
-    path("articles/<int:id>/", ArticleDetails.as_view())
+    path('', include(router.urls)),
+    # path("articles", ArticleList.as_view()),
+    # path("articles/<int:id>/", ArticleDetails.as_view())
 ]
