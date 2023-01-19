d4700038a9f0f77e618f5d29dcf8f31454867f41
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-19 19:07:22 +0100

    chore: backend clean up

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path,include
-# from .views import  ArticleList, ArticleDetails
 from .views import ArticleViewSet, UserViewSet
-from rest_framework import routers
 from rest_framework.routers import DefaultRouter
-# article_list, article_details
 
-router = routers.DefaultRouter()
+
+router = DefaultRouter()
 router.register(r'articles', ArticleViewSet, basename='articles')
 router.register(r'users', UserViewSet)
 
