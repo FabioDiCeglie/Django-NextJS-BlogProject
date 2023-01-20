823d58ddf9c994a29ddd4a496f22d83bbe4475e0
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-20 16:15:29 +0100

    chore: fix some padding

--- frontend/pages/login/index.tsx
+++ frontend/pages/login/index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
         </Head>
         <Container>
           <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-            <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+            <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-6">
               <Link href="/" className="hover:underline">
                 Blog.
               </Link>
