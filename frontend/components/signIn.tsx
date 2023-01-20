d5f824e8b93c1bdd430e4ef4e7cbeeae7496684d
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-20 15:02:42 +0100

    chore: created signin component

---
+++ frontend/components/signIn.tsx
@@ -0,0 +1,56 @@
+export default function SignIn() {
+  return (
+    <section className="h-screen">
+      <div className="container px-6 py-12 h-full">
+        <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+          <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+              className="w-full"
+              alt="Phone image"
+            />
+          </div>
+          <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
+            <form>
+              <div className="mb-6">
+                <input
+                  type="text"
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Email address"
+                />
+              </div>
+
+              <div className="mb-6">
+                <input
+                  type="password"
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Password"
+                />
+              </div>
+
+              <div className="flex justify-between items-center mb-6"></div>
+
+              <button
+                type="submit"
+                className="inline-block px-7 py-3 bg-black text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light"
+              >
+                Sign in
+              </button>
+              <p className="text-sm font-semibold mt-4 pt-1 mb-0 ">
+                Don't have an account?
+                <a
+                  href="#!"
+                  className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out ml-2"
+                >
+                  Register
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ frontend/pages/login/index.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import Container from "../../components/container";
+import Layout from "../../components/layout";
+import SignIn from "../../components/signIn";
+import Link from "next/link";
+
+export default function Index() {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Log In</title>
+        </Head>
+        <Container>
+          <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+            <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+              <Link href="/" className="hover:underline">
+                Blog.
+              </Link>
+            </h1>
+          </section>
+          <SignIn />
+        </Container>
+      </Layout>
+    </>
+  );
+}
