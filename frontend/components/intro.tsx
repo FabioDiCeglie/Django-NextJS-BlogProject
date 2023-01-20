e5ed713a5cf83c166a3a7f22efdefc6b227137d1
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-20 14:52:11 +0100

    chore: created Log In Button

--- frontend/components/intro.tsx
+++ frontend/components/intro.tsx
@@ -4,16 +4,12 @@ const Intro = () => {
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and Python.
-      </h4>
+      <button
+        type="button"
+        className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Log in
+      </button>
     </section>
   );
 };
