e53798288c343c201d801d2799c67ac4dde7ed9b
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-25 21:07:00 +0100

    chore: working on hydratation error

--- frontend/components/intro.tsx
+++ frontend/components/intro.tsx
@@ -1,30 +1,33 @@
 import Link from "next/link";
+import { useEffect } from "react";
 import { useCookies } from "react-cookie";
+import { useRouter } from "next/router";
 
 const Intro = () => {
+  const router = useRouter();
   const [token, setToken, removeToken] = useCookies(["mytoken"]);
+  useEffect(() => {
+    if (token["mytoken"]) {
+      router.push("/");
+    }
+  }, [token]);
+
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      {!token["mytoken"] ? (
+      {token["mytoken"] ? (
         <button
-          type="button"
           className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          onClick={() => removeToken("mytoken")}
         >
-          <Link href="/login" className="hover:underline">
-            Log in
-          </Link>
+          Log out
         </button>
       ) : (
-        <button
-          type="button"
-          className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-          onClick={() => removeToken(["mytoken"])}
-        >
-          <Link href="/" className="hover:underline">
-            Log out
+        <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+          <Link href="/login" className="hover:underline">
+            Log in
           </Link>
         </button>
       )}
