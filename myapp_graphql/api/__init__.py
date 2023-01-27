06f1468d9304f9be8fb991b9e7bdab66580b220c
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-27 15:08:25 +0100

    chore: adding postgressql instance

--- myapp_graphql/api/__init__.py
+++ myapp_graphql/api/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://sjhubmio:5iImset5RSdIfKQDtybKZf1BiEafIXNT@snuffleupagus.db.elephantsql.com/sjhubmio"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
