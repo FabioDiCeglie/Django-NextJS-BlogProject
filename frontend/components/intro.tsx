5db7ae161f309ccb5bc7a822c7812b5046c06144
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-27 14:55:00 +0100

    chore: fixed hydratation error

--- frontend/components/intro.tsx
+++ frontend/components/intro.tsx
@@ -1,30 +1,46 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 
 const Intro = () => {
   const [token, setToken, removeToken] = useCookies(["mytoken"]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
-      {token["mytoken"] ? (
+  useEffect(() => {
+    if (token["mytoken"]) {
+      setIsLoggedIn(true);
+    }
+  }, [isLoggedIn]);
+
+  if (isLoggedIn) {
+    return (
+      <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+        <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+          Blog.
+        </h1>
         <button
           className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-          onClick={() => removeToken("mytoken")}
+          onClick={() => {
+            removeToken("mytoken");
+            setIsLoggedIn(false);
+          }}
         >
           Log out
         </button>
-      ) : (
+      </section>
+    );
+  } else {
+    return (
+      <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+        <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+          Blog.
+        </h1>
         <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-          <Link href="/login" className="hover:underline">
-            Log in
-          </Link>
+          <Link href="/login">Log in</Link>
         </button>
-      )}
-    </section>
-  );
+      </section>
+    );
+  }
 };
 
 export default Intro;
