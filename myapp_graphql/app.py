fd47536bd26a6a64a1f3578f44a3a3b1aec7d5a8
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-27 15:41:00 +0100

    chore: created graphql endpoint with ariadne

--- myapp_graphql/app.py
+++ myapp_graphql/app.py
@@ -1,6 +1,30 @@
 from api import app, db
 from api import models
+from ariadne import load_schema_from_path, make_executable_schema, \
+    graphql_sync, snake_case_fallback_resolvers, ObjectType
+from ariadne.constants import PLAYGROUND_HTML
+from flask import request, jsonify
 
+type_defs = load_schema_from_path("schema.graphql")
+schema = make_executable_schema(
+    type_defs, snake_case_fallback_resolvers
+)
+
+@app.route("/graphql", methods=["GET"])
+def graphql_playground():
+    return PLAYGROUND_HTML, 200
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
 
 # from api.models import Article
 # from datetime import datetime
