4e847d849b55304437962394c5a6e713514f4dc1
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-28 15:33:02 +0100

    chore: connecting django to a postgresSQL

--- django_backend_graphene/backend/settings.py
+++ django_backend_graphene/backend/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tlhoaego',
+        'USER': 'tlhoaego',
+        'PASSWORD': 'Ry1l5kFiPKgumO7LF31BfObcNzJXsiKJ',
+        'HOST': 'hattie.db.elephantsql.com',
+        'PORT': "5432",
     }
 }
 
