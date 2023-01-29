596de9ceb6a9a1b03c9bdb1b3631dce1fff4269b
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-29 22:09:08 +0100

    chore: use secret_key in env

--- django_backend_graphene/backend/settings.py
+++ django_backend_graphene/backend/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import environ
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)+z+$!c!3nwntfutd08jd!b^d&ezfu5sfjh4$m%@(b_(#xgl0t'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -89,9 +93,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #     }
 # }
 
-# Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
 
 DATABASES = {
       'default': {
