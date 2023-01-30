06bedfd85013215a360171faf0f0121c859bf4ba
FabioDiCeglie <dcfabio96@gmail.com>
2023-01-30 14:57:41 +0100

    chore: allow all hosts

--- django_backend_graphene/backend/settings.py
+++ django_backend_graphene/backend/settings.py
@@ -31,10 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-  'localhost',
-  '127.0.0.1',
-  'https://graphql-proj.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
